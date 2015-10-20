f6077aacafc32628ad1f99401b6cac54417c036a
kevin <kevin.hu@jwaoo.com>
2015-10-20 19:43:33 +0800

    update wireless PTT module's code
    Signed-off-by: kevin <kevin.hu@jwaoo.com>

--- wirelessptt/wireless.c
+++ wirelessptt/wireless.c
@@ -27,28 +27,30 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/utsname.h>
-#include "uim.h"
+
 #include <asm-generic/termbits.h>
 #include <asm-generic/ioctls.h>
-#ifdef ANDROID
+
 #include <private/android_filesystem_config.h>
 #include <cutils/log.h>
 #include <dirent.h>
-#endif
 
-#define UIM_SYSFS_SIZE 64
 
-char DEV_NAME_SYSFS[UIM_SYSFS_SIZE];
+#define W_SYSFS_SIZE 64
 
-char _SYSFS[UIM_SYSFS_SIZE];
-char FLOW_CTRL_SYSFS[UIM_SYSFS_SIZE];
+char DEV_NAME_SYSFS[W_SYSFS_SIZE];
+char SQ_SYSFS[W_SYSFS_SIZE];// noiseSQ_SYSFS
+char VOXDEC_SYSFS[W_SYSFS_SIZE]; //handfree detected
+char PTT_SYSFS[W_SYSFS_SIZE]; // ptt 1=recv, 0=send
+char PD_SYSFS[W_SYSFS_SIZE];  //0=sleep
+char HL_SYSFS[W_SYSFS_SIZE]; // 0=0.5w, 1=1w
 
 #define LOG_TAG "wireless_ptt"
-#define UIM_ERR(fmt, arg...)  ALOGE("wireless:"fmt"\n" , ##arg)
+#define RFS_ERR(fmt, arg...)  ALOGE("wireless:"fmt"\n" , ##arg)
 
-#define UIM_START_FUNC()      ALOGE("wireless: Inside %s", __FUNCTION__)
-#define UIM_DBG(fmt, arg...)  ALOGE("wireless:"fmt"\n" , ## arg)
-#define UIM_VER(fmt, arg...)  ALOGE("wireless:"fmt"\n" , ## arg)
+#define RFS_START_FUNC()      ALOGE("wireless: Inside %s", __FUNCTION__)
+#define RFS_DBG(fmt, arg...)  ALOGE("wireless:"fmt"\n" , ## arg)
+#define RFS_INFO(fmt, arg...)  ALOGE("wireless:"fmt"\n" , ## arg)
 
 
 /* Maintains the exit state of UIM*/
@@ -56,6 +58,96 @@ static int exiting;
 
 /* File descriptor for the UART device*/
 int dev_fd;
+
+static void set_speek_dir(char *dir)
+{
+	char val;
+	int sval = 0;
+	int fd = open((const char*) PTT_SYSFS, O_RDWR);
+	
+	if (!memcmp(dir, "SEND", 4)){
+		val = '0';
+	}else if (!memcmp(dir, "RECV", 4)){
+		val = '1';
+	}else {
+		RFS_ERR("dir %s error!!!", dir);
+		return;
+	}
+	sval = (int)val;
+	write(fd, &val, 4);
+	close(fd);
+}
+
+static void set_power(char *power)
+{
+	char val;
+	int sval = 0;
+	int fd = open((const char*) HL_SYSFS, O_RDWR);
+	
+	if (!memcmp(power, "LOW", 4)){
+		val = '0';
+	}else if (!memcmp(power, "HIGH", 4)){
+		val = '1';
+	}else {
+		RFS_ERR("power %s error!!!", power);
+		return;
+	}
+	sval = (int)val;
+	write(fd, &val, 4);
+	close(fd);
+}
+
+static void set_sleep(char *sleep)
+{
+	char val;
+	int sval = 0;
+	int fd = open((const char*) PD_SYSFS, O_RDWR);
+	
+	if (!memcmp(sleep, "SLEEP", 4)){
+		val = '0';
+	}else if (!memcmp(sleep, "WAKE", 4)){
+		val = '1';
+	}else {
+		RFS_ERR("sleep %s error!!!", sleep);
+		return;
+	}
+	sval = (int)val;
+	write(fd, &val, 4);
+	close(fd);
+}
+
+static void set_sqiut(char *sq)
+{
+	char val;
+	int sval = 0;
+	int fd = open((const char*) SQ_SYSFS, O_RDWR);
+	
+	if (!memcmp(sq, "ON", 4)){
+		val = '0';
+	}else if (!memcmp(sq, "OFF", 4)){
+		val = '1';
+	}else {
+		RFS_ERR("sq %s error!!!", sq);
+		return;
+	}
+	sval = (int)val;
+	write(fd, &val, 4);
+	close(fd);
+}
+
+static int set_vox_detect()
+{
+	char buf[4];
+	int val = 0;
+	int fd = open((const char*) VOXDEC_SYSFS, O_RDONLY);
+	
+	if (read(fd, buf, 4) != 4)
+		return 0;
+	
+	sscanf(buf, "%d", &val);
+	
+	return val;
+}
 
 static inline void cleanup(int failed)
 {
@@ -65,7 +157,7 @@ static inline void cleanup(int failed)
 	if (dev_fd == -1)
 		return;
 
-	UIM_DBG("%s", __func__);
+	RFS_DBG("%s", __func__);
 
 	close(dev_fd);
 	dev_fd = -1;
@@ -73,7 +165,6 @@ static inline void cleanup(int failed)
 }
 
 /*****************************************************************************/
-#ifdef UIM_DEBUG
 /*  Function to Read the firmware version
  *  module into the system. Currently used for
  *  debugging purpose, whenever the baud rate is changed
@@ -84,17 +175,15 @@ void read_firmware_version()
 	char resp_buffer[20] = { 0 };
 	unsigned char buffer[] = { 0x01, 0x01, 0x10, 0x00 };
 
-	UIM_START_FUNC();
-	UIM_VER(" wrote %d bytes", (int)write(dev_fd, buffer, 4));
-	UIM_VER(" reading %d bytes", (int)read(dev_fd, resp_buffer, 15));
+	RFS_START_FUNC();
+	RFS_INFO(" wrote %d bytes", (int)write(dev_fd, buffer, 4));
+	RFS_INFO(" reading %d bytes", (int)read(dev_fd, resp_buffer, 15));
 
 	for (index = 0; index < 15; index++)
-		UIM_VER(" %x ", resp_buffer[index]);
+		RFS_INFO(" %x ", resp_buffer[index]);
 
 	printf("\n");
 }
-#endif /* UIM_DEBUG */
-
 
 /* Function to set the default baud rate
  *
@@ -112,7 +201,7 @@ static int uart_config()
 
 	/* Get the attributes of UART */
 	if (tcgetattr(dev_fd, &ti) < 0) {
-		UIM_ERR(" Can't get port settings");
+		RFS_ERR(" Can't get port settings");
 		return -1;
 	}
 
@@ -134,7 +223,7 @@ static int uart_config()
 	tcsetattr(dev_fd, TCSANOW, &ti);
 
 	tcflush(dev_fd, TCIOFLUSH);
-	UIM_DBG(" set_baud_rate() done");
+	RFS_DBG(" set_baud_rate() done");
 
 	return 0;
 }
@@ -146,18 +235,18 @@ static int uart_config()
  * The baud rate is then changed to custom baud rate by this function*/
 static int set_custom_baud_rate(int cust_baud_rate, unsigned char flow_ctrl)
 {
-	UIM_START_FUNC();
+	RFS_START_FUNC();
 
 	struct termios ti;
 	struct termios2 ti2;
 
 	/* Get the attributes of UART */
 	if (tcgetattr(dev_fd, &ti) < 0) {
-		UIM_ERR(" Can't get port settings");
+		RFS_ERR(" Can't get port settings");
 		return -1;
 	}
 
-	UIM_VER(" Changing baud rate to %u, flow control to %u",
+	RFS_INFO(" Changing baud rate to %u, flow control to %u",
 			cust_baud_rate, flow_ctrl);
 
 	/* Flush non-transmitted output data,
@@ -175,7 +264,7 @@ static int set_custom_baud_rate(int cust_baud_rate, unsigned char flow_ctrl)
 	 * The change will occur immediately by using TCSANOW
 	 */
 	if (tcsetattr(dev_fd, TCSANOW, &ti) < 0) {
-		UIM_ERR(" Can't set port settings");
+		RFS_ERR(" Can't set port settings");
 		return -1;
 	}
 
@@ -188,7 +277,7 @@ static int set_custom_baud_rate(int cust_baud_rate, unsigned char flow_ctrl)
 	ti2.c_ospeed = cust_baud_rate;
 	ioctl(dev_fd, TCSETS2, &ti2);
 
-	UIM_DBG(" set_custom_baud_rate() done");
+	RFS_DBG(" set_custom_baud_rate() done");
 	return 0;
 }
 
@@ -206,12 +295,12 @@ int uart_open()
 	memset(buf, 0, 32);
 	fd = open(DEV_NAME_SYSFS, O_RDONLY);
 	if (fd < 0) {
-		UIM_ERR("Can't open %s", DEV_NAME_SYSFS);
+		RFS_ERR("Can't open %s", DEV_NAME_SYSFS);
 		return -1;
 	}
 	len = read(fd, buf, 32);
 	if (len < 0) {
-		UIM_ERR("read err (%s)", strerror(errno));
+		RFS_ERR("read err (%s)", strerror(errno));
 		close(fd);
 		return len;
 	}
@@ -220,12 +309,12 @@ int uart_open()
 
 
 	if (dev_fd != -1) {
-		UIM_ERR("opening %s, while already open", uart_dev_name);
+		RFS_ERR("opening %s, while already open", uart_dev_name);
 		cleanup(-1);
 	}
 	dev_fd = open((const char*) uart_dev_name, O_RDWR);
 	if (dev_fd < 0) {
-		UIM_ERR(" Can't open %s", uart_dev_name);
+		RFS_ERR(" Can't open %s", uart_dev_name);
 		return -1;
 	}
 	/*
@@ -233,7 +322,7 @@ int uart_open()
 	 * This will set the baud rate to default 115200
 	 */
 	if (uart_config() < 0) {
-		UIM_ERR(" set_baudrate() failed");
+		RFS_ERR(" set_baudrate() failed");
 		cleanup(-1);
 		return -1;
 	}
@@ -261,7 +350,7 @@ int main(int argc, char *argv[])
 	int i, n;
 	struct dirent **namelist;
 
-	UIM_START_FUNC();
+	RFS_START_FUNC();
 	err = 0;
 
 	n = scandir("/sys/devices", &namelist, dir_filter, alphasort);
@@ -279,154 +368,54 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	UIM_DBG("sr_frs sysfs name: %s", namelist[0]->d_name);
-	snprintf(INSTALL_SYSFS_ENTRY, UIM_SYSFS_SIZE, "/sys/devices/%s/install", namelist[0]->d_name);
-	snprintf(DEV_NAME_SYSFS, UIM_SYSFS_SIZE, "/sys/devices/%s/dev_name", namelist[0]->d_name);
-	snprintf(BAUD_RATE_SYSFS, UIM_SYSFS_SIZE, "/sys/devices/%s/baud_rate", namelist[0]->d_name);
-	snprintf(FLOW_CTRL_SYSFS, UIM_SYSFS_SIZE, "/sys/devices/%s/flow_cntrl", namelist[0]->d_name);
+	RFS_DBG("sr_frs sysfs name: %s", namelist[0]->d_name);
+	snprintf(DEV_NAME_SYSFS, W_SYSFS_SIZE, "/sys/devices/%s/dev_name", namelist[0]->d_name);
+	snprintf(SQ_SYSFS, W_SYSFS_SIZE, "/sys/devices/%s/sq_gpios", namelist[0]->d_name);
+	snprintf(PD_SYSFS, W_SYSFS_SIZE, "/sys/devices/%s/pd_gpios", namelist[0]->d_name);
+	snprintf(PTT_SYSFS, W_SYSFS_SIZE, "/sys/devices/%s/ptt_gpios", namelist[0]->d_name);
+	snprintf(HL_SYSFS, W_SYSFS_SIZE, "/sys/devices/%s/hl_gpios", namelist[0]->d_name);
+	snprintf(VOXDEC_SYSFS, W_SYSFS_SIZE, "/sys/devices/%s/voxdec_gpios", namelist[0]->d_name);
 
 	free(namelist[0]);
 	free(namelist);
 
-#ifndef ANDROID
-	if (uname (&name) == -1) {
-		UIM_ERR("cannot get kernel release name");
+	if (0 == lstat(DEV_NAME_SYSFS, &file_stat)) {
+		RFS_DBG("File %s is ok\n", DEV_NAME_SYSFS);
+	} else {
+		RFS_DBG("File %s is not found\n", DEV_NAME_SYSFS);
 		return -1;
 	}
-#else  /* if ANDROID */
-
-	if (0 == lstat("/st_drv.ko", &file_stat)) {
-		if (insmod("/st_drv.ko", "") < 0) {
-			UIM_ERR(" Error inserting st_drv module");
-			return -1;
-		} else {
-			UIM_DBG(" Inserted st_drv module");
-		}
+	if (0 == lstat(SQ_SYSFS, &file_stat)) {
+		RFS_DBG("File %s is ok\n", SQ_SYSFS);
 	} else {
-		if (0 == lstat(INSTALL_SYSFS_ENTRY, &file_stat)) {
-			UIM_DBG("ST built into the kernel ?");
-		} else {
-			UIM_ERR("BT/FM/GPS would be unavailable on system");
-			return -1;
-		}
-	}
-
-	if (0 == lstat("/btwilink.ko", &file_stat)) {
-		if (insmod("/btwilink.ko", "") < 0) {
-			UIM_ERR(" Error inserting btwilink module, NO BT? ");
-		} else {
-			UIM_DBG(" Inserted btwilink module");
-		}
-	} else {
-		UIM_DBG("BT driver module un-available... ");
-		UIM_DBG("BT driver built into the kernel ?");
-	}
-
-	if (0 == lstat("/fm_drv.ko", &file_stat)) {
-		if (insmod("/fm_drv.ko", "") < 0) {
-			UIM_ERR(" Error inserting fm_drv module, NO FM? ");
-		} else {
-			UIM_DBG(" Inserted fm_drv module");
-		}
-	} else {
-		UIM_DBG("FM driver module un-available... ");
-		UIM_DBG("FM driver built into the kernel ?");
-	}
-
-	if (0 == lstat("/gps_drv.ko", &file_stat)) {
-		if (insmod("/gps_drv.ko", "") < 0) {
-			UIM_ERR(" Error inserting gps_drv module, NO GPS? ");
-		} else {
-			UIM_DBG(" Inserted gps_drv module");
-		}
-	} else {
-		UIM_DBG("GPS driver module un-available... ");
-		UIM_DBG("GPS driver built into the kernel ?");
-	}
-
-	if (0 == lstat("/fm_v4l2_drv.ko", &file_stat)) {
-		if (insmod("/fm_v4l2_drv.ko", "") < 0) {
-			UIM_ERR(" Error inserting fm_v4l2_drv module, NO FM? ");
-		} else {
-			UIM_DBG(" Inserted fm_v4l2_drv module");
-		}
-	} else {
-		UIM_DBG("FM V4L2 driver module un-available... ");
-		UIM_DBG("FM V4L2 driver built into the kernel ?");
-	}
-	/* Change the permissions for v4l2 Fm device node */
-	if ((0 == lstat("/dev/radio0", &file_stat)) && chmod("/dev/radio0", 0666) < 0) {
-		UIM_ERR("unable to chmod /dev/radio0, might not exist");
-	}
-	if ((0 == lstat("/dev/tifm", &file_stat)) && chmod("/dev/tifm", 0666) < 0) {
-		UIM_ERR("unable to chmod /dev/tifm, might not exist");
-	}
-	/* change rfkill perms after insertion of BT driver which asks
-	 * the Bluetooth sub-system to create the rfkill device of type
-	 * "bluetooth"
-	 */
-	if (change_rfkill_perms() < 0) {
-		/* possible error condition */
-		UIM_ERR("rfkill not enabled in st_drv - BT on from UI might fail\n");
-	}
-
-#endif /* ANDROID */
-	/* rfkill device's open/poll/read */
-	st_fd = open(INSTALL_SYSFS_ENTRY, O_RDONLY);
-	if (st_fd < 0) {
-		UIM_DBG("unable to open %s (%s)", INSTALL_SYSFS_ENTRY,
-				strerror(errno));
-		remove_modules();
+		RFS_DBG("File %s is not found\n", SQ_SYSFS);
 		return -1;
 	}
-
-RE_POLL:
-	err = read(st_fd, &install, 1);
-	if ((err > 0) && (install == '1')) {
-		UIM_DBG("install already set");
-		st_uart_config(install);
-	}
-
-	memset(&p, 0, sizeof(p));
-	p.fd = st_fd;
-	/* sysfs entries can only break poll for following events */
-	p.events = POLLERR | POLLHUP;
-
-	while (!exiting) {
-		p.revents = 0;
-		err = poll(&p, 1, -1);
-		if (err < 0 && errno == EINTR)
-			continue;
-		if (err)
-			break;
-	}
-
-	close(st_fd);
-	st_fd = open(INSTALL_SYSFS_ENTRY, O_RDONLY);
-	if (st_fd < 0) {
-		UIM_ERR("re-opening %s failed: %s", INSTALL_SYSFS_ENTRY,
-		strerror(errno));
+	if (0 == lstat(PD_SYSFS, &file_stat)) {
+		RFS_DBG("File %s is ok\n", PD_SYSFS);
+	} else {
+		RFS_DBG("File %s is not found\n", PD_SYSFS);
 		return -1;
 	}
-
-	if (!exiting)
-	{
-		err = read(st_fd, &install, 1);
-		if (err <= 0) {
-			UIM_ERR("reading %s failed: %s", INSTALL_SYSFS_ENTRY,
-					strerror(errno));
-			goto RE_POLL;
-		}
-		st_uart_config(install);
-		goto RE_POLL;
-	}
-
-	if(remove_modules() < 0) {
-		UIM_ERR(" Error removing modules ");
-		close(st_fd);
+	if (0 == lstat(PTT_SYSFS, &file_stat)) {
+		RFS_DBG("File %s is ok\n", PTT_SYSFS);
+	} else {
+		RFS_DBG("File %s is not found\n", PTT_SYSFS);
 		return -1;
 	}
+	if (0 == lstat(HL_SYSFS, &file_stat)) {
+		RFS_DBG("File %s is ok\n", HL_SYSFS);
+	} else {
+		RFS_DBG("File %s is not found\n", HL_SYSFS);
+		return -1;
+	}
+	if (0 == lstat(VOXDEC_SYSFS, &file_stat)) {
+		RFS_DBG("File %s is ok\n", VOXDEC_SYSFS);
+	} else {
+		RFS_DBG("File %s is not found\n", VOXDEC_SYSFS);
+		return -1;
+	}	
 
-	close(st_fd);
+
 	return 0;
 }
