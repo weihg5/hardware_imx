cea0fb7803c3dfe304fe411e89115042032b0cbb
kevin <huhuikevin@gmail.com>
2015-12-06 19:19:39 +0800

    add gps power on&down function
    Signed-off-by: kevin <huhuikevin@gmail.com>

---
+++ ublox_gps/hal/ubx_board.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define GPS_RESET "/sys/class/gpio/gpio192/value" //gpio7 0
+#define GPS_POWER "/sys/class/gpio/gpio84/value" //gpio3 20
+
+static void gpio_write(const char *gpio, int val)
+{
+	int fd = open(gpio, O_RDWR);
+	const char *v = val?"1":"0";
+	if (fd < 0)
+		return;
+
+	write(fd, v, 1);
+
+	close(fd);
+}
+
+static void gps_power(int on)
+{
+	gpio_write(GPS_POWER, on);
+}
+
+
+static void gps_reset()
+{
+	gpio_write(GPS_RESET, 0);
+	usleep(1000*20);
+	gpio_write(GPS_RESET, 1);
+}
+
