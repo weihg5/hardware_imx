710127714f01047eb149a0e5987eda7041bde902
Fuang.Cao <cavan.cfa@gmail.com>
2016-02-21 16:54:31 +0800

    fixup IN4R route fault
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- wirelessptt/wireless.c
+++ wirelessptt/wireless.c
@@ -48,7 +48,8 @@
 
 #include <dirent.h>
 
-#define RFS_DEBUG	1
+#define RFS_AUDIO_DEBUG	1
+#define RFS_USE_IN4R	1
 
 #define W_SYSFS_SIZE 256
 
@@ -617,46 +618,54 @@ static bool tinymix_command(const char *control, const char *value)
 static void set_audio_route(bool enable)
 {
 	tinymix_command("MIXINR PGA Switch", "0");
-#if RFS_DEBUG
+#if RFS_AUDIO_DEBUG
 	tinymix_command("MIXINL PGA Switch", "0");
 #endif
 
 	if (enable) {
-		tinymix_command("MIXINR IN4R Switch", "1");
-#if RFS_DEBUG
-		tinymix_command("MIXINL IN2L Switch", "1");
+#if RFS_USE_IN4R
+		tinymix_command("HPMIXL IN4R Switch", "1");
+		tinymix_command("SPKOUTL Mixer IN4R Switch", "1");
+#else
+		tinymix_command("MIXINR IN3R Switch", "1");
+		tinymix_command("HPMIXL MIXINR Switch", "1");
+		tinymix_command("SPKOUTL Mixer MIXINR Switch", "1");
 #endif
 
-		tinymix_command("HPMIXL MIXINR Switch", "1");
-#if RFS_DEBUG
+#if RFS_AUDIO_DEBUG
+		tinymix_command("MIXINL IN2L Switch", "1");
 		tinymix_command("HPMIXL MIXINL Switch", "1");
+		tinymix_command("SPKOUTL Mixer MIXINL Switch", "1");
 #endif
+
 		tinymix_command("HPOUTL PGA", "Mixer");
 		tinymix_command("Headphone Mixer Switch", "1");
 		tinymix_command("Headphone Volume", "121");
 		tinymix_command("Headphone Switch", "1");
 
-		tinymix_command("SPKOUTL Mixer MIXINR Switch", "1");
-#if RFS_DEBUG
-		tinymix_command("SPKOUTL Mixer MIXINL Switch", "1");
-#endif
 		tinymix_command("SPKOUTL PGA", "Mixer");
 		tinymix_command("Speaker Mixer Switch", "1");
 		tinymix_command("Speaker Switch", "1");
 		tinymix_command("Speaker Volume", "121");
 	} else {
-		tinymix_command("MIXINR IN4R Switch", "0");
-#if RFS_DEBUG
+#if RFS_USE_IN4R
+		tinymix_command("HPMIXL IN4R Switch", "0");
+		tinymix_command("SPKOUTL Mixer IN4R Switch", "0");
+#else
+		tinymix_command("MIXINR IN3R Switch", "0");
+		tinymix_command("HPMIXL MIXINR Switch", "0");
+		tinymix_command("SPKOUTL Mixer MIXINR Switch", "0");
+#endif
+
+#if RFS_AUDIO_DEBUG
 		tinymix_command("MIXINL IN2L Switch", "0");
 #endif
 
 		tinymix_command("Headphone Mixer Switch", "0");
 		tinymix_command("Headphone Mixer Switch", "0");
-		tinymix_command("HPMIXL MIXINR Switch", "0");
 		tinymix_command("HPOUTL PGA", "DAC");
 
 		tinymix_command("Speaker Mixer Switch", "0");
-		tinymix_command("SPKOUTL Mixer MIXINR Switch", "0");
 		tinymix_command("SPKOUTL PGA", "DAC");
 		// tinymix_command("Headphone Volume", "0");
 		// tinymix_command("Headphone Switch", "0");
